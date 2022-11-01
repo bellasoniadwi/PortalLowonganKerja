3c6fce60eb0a1fd1b10ced7c86bbbbfe81fe393b
bellasoniadwi <bellasoniadwi@gmail.com>
2022-11-01 19:55:59 +0700

    edit perhitungan isi card dashboard

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -15,7 +15,7 @@
                 </div>
                 <div class="card-wrap" href="{{ route('titik.index') }}">
                     <div class="card-header">
-                        <h4>Peta</h4>
+                        <h4>Lokasi</h4>
                     </div>
                     <div class="card-body">
                         {{ App\Models\Peta::count() }}
@@ -30,10 +30,10 @@
                 </div>
                 <div class="card-wrap" href="{{ route('toko.index') }}">
                     <div class="card-header">
-                        <h4>Perusahaan</h4>
+                        <h4>Lowongan</h4>
                     </div>
                     <div class="card-body">
-                        {{ App\Models\Toko::count() }}
+                        {{ App\Models\LowonganPekerjaan::count() }}
                     </div>
                 </div>
             </div>
@@ -43,47 +43,32 @@
                 <div class="card-icon bg-warning">
                     <i class="far fa-file"></i>
                 </div>
-                <div class="card-wrap" href="{{ route('layanan.index') }}">
+                <div class="card-wrap">
                     <div class="card-header">
-                        <h4>Layanan</h4>
+                        <h4>Kategori</h4>
                     </div>
                     <div class="card-body">
-                        {{ App\Models\Layanan::count() }}
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-            <div class="card card-statistic-1">
-                <div class="card-icon bg-success">
-                    <i class="fas fa-circle"></i>
-                </div>
-                <div class="card-wrap" href="{{ route('transaksi.index') }}">
-                    <div class="card-header">
-                        <h4>Transaksi</h4>
-                    </div>
-                    <div class="card-body">
-                        {{ App\Models\Transaksi::count() }}
+                        {{ App\Models\Kategori::count() }}
                     </div>
                 </div>
             </div>
         </div>
         @can('admin')
-            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-                <div class="card card-statistic-1">
-                    <div class="card-icon bg-success">
-                        <i class="fas fa-circle"></i>
+        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+            <div class="card card-statistic-1">
+                <div class="card-icon bg-success">
+                    <i class="fas fa-circle"></i>
+                </div>
+                <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Admin</h4>
                     </div>
-                    <div class="card-wrap" href="{{ route('status.index') }}">
-                        <div class="card-header">
-                            <h4>Status</h4>
-                        </div>
-                        <div class="card-body">
-                            {{ App\Models\Status::count() }}
-                        </div>
+                    <div class="card-body">
+                        {{ App\Models\User::count() }}
                     </div>
                 </div>
             </div>
+        </div>
         @endcan
     </div>
 @endsection
