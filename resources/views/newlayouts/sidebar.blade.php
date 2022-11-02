c353316584b61b7654954b2f1bccef4c03e2f863
Ayi <ayianjani774@gmail.com>
2022-11-02 11:27:01 +0700

    update sidebar

--- resources/views/newlayouts/sidebar.blade.php
+++ resources/views/newlayouts/sidebar.blade.php
@@ -2,21 +2,21 @@
     <div class="main-sidebar sidebar-style-2">
         <aside id="sidebar-wrapper">
             <div class="sidebar-brand">
-                <a href="index.html">Portal</a>
+                <a href="{{route('home')}}">Portal</a>
             </div>
             <div class="sidebar-brand sidebar-brand-sm">
                 <a href="index.html">Pl</a>
             </div>
             <ul class="sidebar-menu">
                 <li class="menu-header">Dashboard</li>
-                <li class="@yield('home')"><a class="nav-link" href="{{route('home')}}"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
+                <li class="@yield('home')"><a class="nav-link" href="{{route('home')}}"><i class="fas fa-desktop"></i> <span>Dashboard</span></a></li>
 
                 <li class="menu-header">Main Menu</li>
                 <li class="@yield('peta')"><a class="nav-link" href="{{ route('titik.index') }}"><i class="fas fa-map-marker-alt"></i> <span>Peta</span></a></li>
                 @can('admin')
                 <li class="@yield('perusahaan')"><a class="nav-link" href="{{ route('perusahaan') }}"><i class="fas fa-th-large"></i> <span>Perusahaan</span></a></li>
                 @endcan
-                <li class="@yield('lowonganpekerjaan')"><a class="nav-link" href="{{ route('lowonganpekerjaan.index') }}"><i class="fas fa-plug"></i> <span>Lowongan Pekerjaan</span></a></li>
+                <li class="@yield('lowonganpekerjaan')"><a class="nav-link" href="{{ route('lowonganpekerjaan.index') }}"><i class="far fa-newspaper"></i> <span>Lowongan Pekerjaan</span></a></li>
                 @can('admin')
                 <li class="@yield('kategori')"><a class="nav-link" href="{{ route('kategori.index') }}"><i class="fas fa-ellipsis-h"></i> <span>Kategori</span></a></li>
                 @endcan
