4091aa34053793b92a155b28adf9725881924747
bellasoniadwi <bellasoniadwi@gmail.com>
2022-11-04 15:14:33 +0700

    update sidebar

--- resources/views/newlayouts/sidebar.blade.php
+++ resources/views/newlayouts/sidebar.blade.php
@@ -16,7 +16,10 @@
                 @can('admin')
                 <li class="@yield('perusahaan')"><a class="nav-link" href="{{ route('perusahaan') }}"><i class="fas fa-city"></i> <span>Perusahaan</span></a></li>
                 @endcan
-                <li class="@yield('lowonganpekerjaan')"><a class="nav-link" href="{{ route('lowonganpekerjaan.index') }}"><i class="far fa-newspaper"></i> <span>Lowongan Pekerjaan</span></a></li>
+                <li class="@yield('lowonganpekerjaan')"><a class="nav-link" href="{{ route('lowonganpekerjaan.index') }}"><i class="far fa-newspaper"></i> <span>Lowongan Aktif</span></a></li>
+                
+                <li class="@yield('lowonganpekerjaan-inactive')"><a class="nav-link" href="{{ route('lowonganpekerjaan.inactive') }}"><i class="fas fa-book"></i> <span> Arsip Lowongan</span></a></li>
+                
                 @can('admin')
                 <li class="@yield('kategori')"><a class="nav-link" href="{{ route('kategori.index') }}"><i class="fas fa-clipboard-list"></i> <span>Kategori</span></a></li>
                 @endcan
