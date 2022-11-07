33335989948ba9a132d03705ea6808b4efae86f6
bellasoniadwi <bellasoniadwi@gmail.com>
2022-11-07 16:49:55 +0700

    ganti nama variabel

--- app/Http/Middleware/CheckBanned.php
+++ app/Http/Middleware/CheckBanned.php
@@ -17,7 +17,7 @@ class CheckBanned
      */
     public function handle(Request $request, Closure $next)
     {
-        if(auth()->check() && (auth()->user()->status == 0)){
+        if(auth()->check() && (auth()->user()->is_active == 0)){
             Auth::logout();
 
             $request->session()->invalidate();
