2b92605921afe897040e4a7d4e2f28d2d83a2b8d
bellasoniadwi <bellasoniadwi@gmail.com>
2022-11-10 22:55:17 +0700

    hapus pemanggilan route tidak penting

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -28,7 +28,7 @@
                 <div class="card-icon bg-danger">
                     <i class="far fa-newspaper"></i>
                 </div>
-                <div class="card-wrap" href="{{ route('toko.index') }}">
+                <div class="card-wrap">
                     <div class="card-header">
                         <h4>Lowongan</h4>
                     </div>
@@ -65,6 +65,21 @@
                     </div>
                     <div class="card-body">
                         {{ App\Models\User::where('is_admin', false)->count() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+            <div class="card card-statistic-1">
+                <div class="card-icon bg-primary">
+                    <i class="fas fa-comment-alt"></i>
+                </div>
+                <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Pertanyaan Tanpa Jawaban</h4>
+                    </div>
+                    <div class="card-body">
+                        {{ App\Models\Faq::where('jawaban', '=', null)->count() }}
                     </div>
                 </div>
             </div>
