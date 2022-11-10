5e380bd006f36f3a643091b63177c9d41656552c
bellasoniadwi <bellasoniadwi@gmail.com>
2022-11-10 22:55:31 +0700

    add halaman faq di user

---
+++ resources/views/home/faq.blade.php
@@ -0,0 +1,37 @@
+@extends('newlayouts.main')
+
+@section('faq', 'active')
+
+@section('content')
+<div class="section-header">
+    {{-- <h1>DataTables</h1> --}}
+    <div class="section-header-breadcrumb">
+        {{-- <div class="breadcrumb-item"><a href="{{ route('home') }}"><i class="fas fa-desktop"></i> Dashboard</a></div> --}}
+        <div class="breadcrumb-item"><a><i class="fas fa-address-book"></i> FAQ</a></div>
+    </div>
+</div>
+<div class="section-body">
+    <h2 class="section-title">List Pertanyaan yang Mungkin Membantu Anda</h2>
+<div class="row">
+    <div class="col-12">
+      <div class="activities">
+        @foreach ($faqs as $fq)
+        <div class="activity">
+          <div class="activity-icon bg-primary text-white shadow-primary">
+            <i class="fas fa-comment-alt"></i>
+          </div>
+          <div class="activity-detail">
+            <div class="mb-2">
+              <span class="text-job text-primary">{{ $fq->pertanyaan }}</span>
+              <span class="bullet"></span>
+              {{-- <a class="text-job" href="#">View</a> --}}
+            </div>
+            <p>{{ $fq->jawaban }}</p>
+          </div>
+        </div>
+        @endforeach
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
