d12cd8733af4d4a08d549bf1f6560f28d46354f5
anjanidwilestari <ayianjani774@gmail.com>
2022-11-30 11:50:49 +0700

    hapus seeder lowongan pekerjaan

--- database/seeders/LowonganPekerjaanSeeder.php
+++ database/seeders/LowonganPekerjaanSeeder.php
@@ -14,18 +14,6 @@ class LowonganPekerjaanSeeder extends Seeder
      */
     public function run()
     {
-        LowonganPekerjaan::create([
-            'nama_pekerjaan' => 'Guru Bahasa',
-            'kategori' => 'Pendidikan' ,
-            'tipe_pekerjaan' => 'Full Time',
-            'perusahaan' => 'Ambi Corp',
-            'x' => '-7.899770321098634',
-            'y' => '112.52919676867126',
-            'deskripsi' => 'Butuh 1 orang guru wanita yang berpengalaman mengajar anak SMP, pendidikan minimal S1 Bahasa',
-            'gaji' => '200 ribu per pertemuan',
-            'jam_kerja' => '4 jam/pertemuan',
-            'contact_person' => 'Ambi',
-            'no_telp' => '085123404776',
-        ]);
+        //
     }
 }
